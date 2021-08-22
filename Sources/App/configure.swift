f6385e7edf7351c22ab3fa04c6df414b63503eb7
csdsdasdcwcwadx <87317853+csdsdasdcwcwadx@users.noreply.github.com>
2021-08-23 07:48:29 +0800

    Update configure.swift

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -14,6 +14,7 @@ public func configure(_ app: Application) throws {
     app.migrations.add(CreateUser())
     app.migrations.add(CreateInfo())
     app.migrations.add(CreateUserToken())
+    app.migrations.add(EnablePostGISMigration())
     app.sessions.use(.fluent)
 
     
